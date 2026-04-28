2492313dc20811172d83ddd5ea178c9583b64ed9
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-28 11:26:44 +0600

    navbar section done , marquee also added

---
+++ app/(main)/career/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CareerPage = () => {
+    return (
+        <div>
+            career page
+            
+        </div>
+    );
+};
+
+export default CareerPage;
--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,9 +1,32 @@
+
+import Link from 'next/link';
 import React from 'react';
+import userAvatar from '../../public/assets/user.png';
+import Image from 'next/image';
+import Navlink from './Navlink';
 
 const Navbar = () => {
     return (
-        <div>
-            navbar
+        <div className='flex justify-between container mx-auto gap-4 mt-6'>
+            <div></div>
+           <ul className='flex justify-between items-center gap-3 text-gray-700'>
+            <li>
+                <Navlink href={'/'}>Home</Navlink>
+            </li>
+            <li>
+                <Navlink href={'/about-us'}>About</Navlink>
+            </li>
+            <li>
+                <Navlink href={'/career'}>Career</Navlink>
+            </li>
+           </ul>
+           <div className='flex items-center gap-2'>
+           <Image src={userAvatar} alt='user avatar' width={60} height={60}/>
+           <button className='btn btn-primary'>
+            <Link href={'/login'} className='text-white'>Login</Link>
+           </button>
+
+           </div>
         </div>
     );
 };
