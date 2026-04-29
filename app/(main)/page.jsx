8511ce3af1fc8c0698f041547f0e1e4ad2e7d69d
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-29 19:20:15 +0600

    left sidebar functionality done

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,3 +1,4 @@
+import LeftSideBar from "@/components/shared/homepage/news/LeftSideBar";
 import Image from "next/image";
 async function getCategories() {
     const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
@@ -9,18 +10,7 @@ export default async function Home(){
     const categories = await getCategories();
     return <div className="container mx-auto grid grid-cols-12 gap-4 my-[60px]">
         <div className="col-span-3">
-            <h2 className="font-bold text-lg">All categories</h2>
-
-            
-            <ul className="flex flex-col gap-3 mt-6">
-
-            {
-                categories.news_category.map(category => {
-                    return <li key={category.category_id} className="bg-slate-100 p-2
-                    rounded-md font-bold text-center text-md">{category.category_name}</li>
-                })
-            }
-            </ul>
+            <LeftSideBar categories={categories}></LeftSideBar>
         </div>
         <div className="font-bold text-3xl col-span-6">
             All News
