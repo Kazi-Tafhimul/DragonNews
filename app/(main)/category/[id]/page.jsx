1851afdf796fb9a99896bdb2486edc4262698583
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-29 20:07:34 +0600

    category section done

---
+++ app/(main)/category/[id]/page.jsx
@@ -0,0 +1,49 @@
+import LeftSideBar from '@/components/shared/homepage/news/LeftSideBar';
+import RightSidebar from '@/components/shared/homepage/news/RightSidebar';
+import React from 'react';
+async function getCategories() {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    return data.data;
+
+}
+async function getNewsByCategoriesId(category_id) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
+    const data = await res.json();
+    return data.data;
+
+}
+
+const NewsCategoryPage = async ({params}) => {
+    const {id}= await params;
+    const categories = await getCategories();
+    const news = await getNewsByCategoriesId(id)
+    return (
+        <div>
+            <div className="container mx-auto grid grid-cols-12 gap-4 my-[60px]">
+                    <div className="col-span-3">
+                        <LeftSideBar categories={categories} active_id={id}></LeftSideBar>
+                    </div>
+                    <div className="font-bold col-span-6">
+                        All News
+                        <div className="space-y-4">
+            
+                        {
+                           news.length>0 ? news.map(n => {
+                                return <div key={n._id} className="p-6 rounded-md border"> {n.title}
+                                </div>
+                                   
+                            }): <h2 className='font-bold text-4xl text-center my-7'>No news found</h2>
+                        }
+                        </div>
+                    </div>
+                    <div className="col-span-3">
+                        <RightSidebar></RightSidebar>
+                    </div>
+                </div>
+            
+        </div>
+    );
+};
+
+export default NewsCategoryPage;
--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -7,14 +7,31 @@ async function getCategories() {
     return data.data;
 
 }
+async function getNewsByCategoriesId(category_id) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
+    const data = await res.json();
+    return data.data;
+
+}
 export default async function Home(){
     const categories = await getCategories();
+    const news = await getNewsByCategoriesId("01")
     return <div className="container mx-auto grid grid-cols-12 gap-4 my-[60px]">
         <div className="col-span-3">
             <LeftSideBar categories={categories}></LeftSideBar>
         </div>
-        <div className="font-bold text-3xl col-span-6">
+        <div className="font-bold col-span-6">
             All News
+            <div className="space-y-4">
+
+            {
+                news.map(n => {
+                    return <div key={n._id} className="p-6 rounded-md border"> {n.title}
+                    </div>
+                       
+                })
+            }
+            </div>
         </div>
         <div className="col-span-3">
             <RightSidebar></RightSidebar>
